4fb5beabb6b116cc9930216c362654f42480e043
NoeCanovi <noecanovi@gmail.com>
2023-03-27 09:30:36 +0200

    Added things on interface

--- Interface/prova.py
+++ Interface/prova.py
@@ -12,10 +12,21 @@ def main():
 def signUp():
     return render_template('SignUp.html')
 
-# From signup page, create new account or give errors
-# TODO: sistemare
+
+@app.route('/ErrorLogin')
+def ErrorLogin():
+    err_msg = "Invalid credentials"
+    return render_template('ErrorLogin.html', err_msg = err_msg)
+
+@app.route('/ErrorSignUp')
+def ErrorSignUp():
+    err_msg = "Username already in use"
+    return render_template('ErrorSignUp.html', err_msg = err_msg)
+
+
+# From login page go to start page or gives error
 @app.route('/Start/', methods = ["POST", "GET"])
-def startOrError():
+def startLogin():
     #controlla se c'è il nome, ritorna o una pagina o un'altra (errore)
     if request.method == 'GET':
         return f"The URL /Start is accessed directly. Try going to '/form' to submit form"
@@ -24,40 +35,103 @@ def startOrError():
         # take form results
         form_data = request.form
 
-        #usr = form_data['Username']
-        #psw = form_data['Password']
 
         # check if form is empty
         if form_data["Username"]:
             if form_data["Password"]:
                 
-                # TODO: check if username already exists, nel caso dire cambiare nome username o fare il login
-                
-                # vede se l'username è già stato usato o no
-                # funziona ma forse si può usare direttamente panda diceva la chiara? ma non ho capito come
-                with open('usr.csv', 'rt') as f:
+                # check if username exists
+                with open('database/usr.csv', 'rt') as f:
                     reader = csv.reader(f, delimiter=';')
+                    usr_exist = False
                     for row in reader:
                         if form_data["Username"] == row[0]:
-                            print(form_data["Username"], "is in file")
+                            usr_exist = True
+                            # check if psw is correct
+                            if form_data["Password"] == row[1]:
+                                
+                                # TODO capire cosa vogliamo fare e implementarlo
+                                # apre il csv e per ora prende i risultati di marzo
+                                with open('database/Prova.csv', 'rt') as f:
+                                    reader_prova = csv.reader(f, delimiter=';')
+
+                                    data = []
+                                    data_bar = []
+                                    labels = []
+                                    chart_label = "March Trials: Reaction Time"
+                                    for row in reader_prova:
+                                        if row[6] == "03":
+                                            data.append(row[1])
+                                            data_bar.append(row[0])
+                                            labels.append(row[5])
+                                            
+
+                                return render_template('Start.html', usr = form_data["Username"],  data = data, data_bar = data_bar, labels = labels, chart_label = chart_label)
+                        
+                            else:
+                                msg = "Invalid password"
+                                return render_template('ErrorLogin.html', msg = msg, msg_type = "error")
+                            
+                    # if no username is found, gives error       
+                    if usr_exist is False:
+                        msg = "Invalid credentials"
+                        return render_template('ErrorLogin.html', msg = msg, msg_type = "error")
+
+# From signup page create new credentials and go to login or gives error
+@app.route('/Main/', methods = ["POST", "GET"])
+def startSignUp():
+    
+    if request.method == 'POST':
+        # take form results
+        form_data = request.form
+
+        # check if form is empty
+        if form_data["Username"]:
+            if form_data["Password"]:
+                
+                # check if username exists
+                with open('database/usr.csv', 'rt') as f:
+                    reader = csv.reader(f, delimiter=';')
+                    usr_exist = False
+                    for row in reader:
+                        if form_data["Username"] == row[0]:
+                            usr_exist = True
+        
+                    # if username exists, gives error       
+                    if usr_exist:
+                        err_msg = "Username already in use"
+                        return render_template('ErrorSignUp.html', err_msg = err_msg)
+                    
+                    else:
+                        # if username does not exist, add it to csv file
+                        # transform dict to df to csv
+                        df = pd.DataFrame.from_dict([form_data])
+                        df.to_csv("database/usr.csv", sep=';', mode='a', index=False, header=False)
+
+                        # go to login page
+                        msg = "Account has been created"
+                        return render_template('ErrorLogin.html', msg = msg, msg_type = "message")
+
+@app.route('/Trial/', methods = ["POST", "GET"])
+def startNewTrial():
+    if request.method == 'POST':
+        return render_template('Trial.html')
 
 
-                # if username is not already used, add it to csv file
-                # transform dict to df to csv
-                df = pd.DataFrame.from_dict([form_data])
-                print ("Input DataFrame is:", df)
-                df.to_csv("usr.csv", sep=';', mode='a', index=False, header=False)
+# Prova per capire come funziona il request.form non con il form ma con le opzioni
+# Root endpoint
+@app.route('/StartOr', methods = ["POST", "GET"])
+def charts():
+    if request.method == 'POST':
+        print("OOOOOi")
+        form_data = request.form.get('mycars')
+        print(form_data)
+        return render_template('Trial.html')
 
-                # enter start.html
-                return render_template('Start.html', form_data = form_data)
-            
-        else:
-            #TODO add msg "complete form to continue" e sistemare
-            return render_template('ErrorUsr.html', form_data = form_data)
 
-#TODO From Login page, check if username and psw are correct and go to start trials or gives error
-#@app.route('/mettinomecheserve/', methods = ["POST", "GET"])
-#def mettinomecheservecoerente()
+#TODO: fare funzione che scrive nel file csv i risultati delle prove con gli stimoli. Un file per utente
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
