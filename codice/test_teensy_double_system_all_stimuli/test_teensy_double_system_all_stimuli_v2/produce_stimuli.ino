1198fd7ed13964bc17eb6cb00bc7a2db3a1c8ccf
R_mazze <robertomazzaro@gmail.com>
2023-04-15 18:46:21 +0200

    comunicazione python to arduino

---
+++ codice/test_teensy_double_system_all_stimuli/test_teensy_double_system_all_stimuli_v2/produce_stimuli.ino
@@ -0,0 +1,27 @@
+// produce the selected stimuli for this test
+void produce_stimuli(bool stimulus_sx, bool stimulus_dx, bool visual_stimuli, bool auditory_stimuli, bool tactile_stimuli){
+    if (visual_stimuli == HIGH){
+        digitalWrite(visual_stimulus_led_sx, stimulus_sx);
+        digitalWrite(visual_stimulus_led_dx, stimulus_dx);
+        //Serial.println("Produce visual stimuli");
+    }
+    if (auditory_stimuli == HIGH){
+        if (stimulus_sx == HIGH && stimulus_dx == HIGH){
+            Serial.println("Produce auditory sx dx stimuli");
+        }
+        else if (stimulus_sx == HIGH){
+            Serial.println("Produce auditory sx stimulus");
+        }
+        else if (stimulus_sx == HIGH){
+            Serial.println("Produce auditory dx stimulus");
+        }
+        else{
+            Serial.println("No stimuly to produce");
+        }
+    }
+    if (tactile_stimuli == HIGH){
+        digitalWrite(tactile_stimulus_actuator_sx, stimulus_sx);
+        digitalWrite(tactile_stimulus_actuator_dx, stimulus_dx);
+        //Serial.println("Produce tactile stimuli");
+    }  
+}
